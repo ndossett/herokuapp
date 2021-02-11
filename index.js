665bedd91f71167cb5e5021d49ff7f2bf8ddb027
ndossett <niki.m.dossett@gmail.com>
2021-02-11 13:04:55 -0500

    first push

---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors")
+
+const app = express();
+const port = process.env.PORT || 9000; // if no env variable, then whatever you set will be default backup port
+
+app.use(cors());
+app.use(express.json())
+
+app.use("/api/*", (_, res) => {
+    res.json({data: "The API lives!!!"})
+})
+
+app.listen(port, () => {
+    console.log(`Server is alive on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+// console.log("It's alive!");
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.env.USERNAME);
+// console.log(process.env.PORT);
+// console.log(dotenv);
+// console.log(process.env.FOOD);
